68698e3cb0e6c750101ba86c877b9edf813d47b6
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-07-30 18:42:31 +0000

    HTTP, maybe?

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -54,7 +54,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
 
         
         if (this.characterBookPath.length > 0) {
-            const response = await fetch(`https://api.chub.ai/api/characters/download`, {
+            const response = await fetch(`http://api.chub.ai/api/characters/download`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
